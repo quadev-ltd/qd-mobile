f5b61bf3b547cf3d2b3a7f9a46f893e65fe70df2
gustavo-m-franco <gusfran17@gmail>
2024-05-07 21:42:59 +0100

    Add reactotron

--- src/core/state/store.ts
+++ src/core/state/store.ts
@@ -10,6 +10,7 @@ import { apiSlice } from '../api';
 import { generateMMKVStorage } from './mmkv';
 import { authSlice } from './slices/authSlice';
 import { userSlice } from './slices/userSlice';
+
 import reactotron from '@/reactotron/reactotronConfig';
 
 const middlewares: Middleware[] = [];
@@ -37,8 +38,10 @@ export const generateStore = (encriptionKey: string) => {
     reducer: persistedReducer,
     middleware: getDefaultMiddleware =>
       getDefaultMiddleware({ serializableCheck: false }).concat(middlewares),
-    enhancers: (getDefaultEnhancers) =>
-      __DEV__ ? getDefaultEnhancers().concat(reactotron.createEnhancer()) : getDefaultEnhancers(),
+    enhancers: getDefaultEnhancers =>
+      __DEV__
+        ? getDefaultEnhancers().concat(reactotron.createEnhancer())
+        : getDefaultEnhancers(),
   });
   const persistor = persistStore(store);
   return { store, persistor };
--- src/reactotron/reactotronConfig.ts
+++ src/reactotron/reactotronConfig.ts
@@ -1,14 +1,13 @@
-import Reactotron, { networking } from "reactotron-react-native";
-import { reactotronRedux } from 'reactotron-redux'
+import Reactotron, { networking } from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
 
 import { env } from '@/core/env';
 
-const reactotron = Reactotron
-  .configure({ name: env.APPLICATION_NAME })
-  // @ts-expect-error
+const reactotron = Reactotron.configure({ name: env.APPLICATION_NAME })
+  // @ts-expect-error - We are using the networking feature
   .use(networking())
   .useReactNative({ asyncStorage: false })
   .use(reactotronRedux())
-  .connect()
+  .connect();
 
-  export default reactotron;
+export default reactotron;
