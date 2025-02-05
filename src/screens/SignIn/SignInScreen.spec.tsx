f1df01ceec575505829b0f469b870cca85e76828
gustavo-m-franco <gusfran17@gmail>
2025-02-05 23:36:51 +0000

    linting fix

--- src/screens/SignIn/SignInScreen.spec.tsx
+++ src/screens/SignIn/SignInScreen.spec.tsx
@@ -29,6 +29,7 @@ jest.mock('@react-native-firebase/crashlytics', () => ({
   log: jest.fn(),
   recordError: jest.fn(),
 }));
+jest.mock('@react-native-firebase/auth', () => 'Auth');
 jest.mock(
   'react-native-vector-icons/MaterialCommunityIcons',
   () => 'MaterialCommunityIcons',
